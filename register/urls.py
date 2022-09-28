6f84652aafa90fde85962bd4f5edba8452145db8
Hammad <hammadfaiz172@gmail.com>
2022-09-28 10:57:33 -0400

    signup done

--- register/urls.py
+++ register/urls.py
@@ -2,5 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('',views.index, name="index")
+    path('',views.index, name="index"),
+    path('register/', views.register, name="register"),
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout")
 ]
--- register/views.py
+++ register/views.py
@@ -1,5 +1,51 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import UserCreationForm
+# from .models import views
+
+
+from django.contrib import messages
+
+from .forms import CreateUserForm
 
 # Create your views here.
 def index(response):
     return HttpResponse("eawr")
+    
+def register(request):
+    form = CreateUserForm()
+    
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user) 
+            return redirect('login')
+
+
+    context = {'form': form}
+    return render(request, 'register/register.html', context)
+
+def loginPage(request):
+    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('http://127.0.0.1:8000/main/')
+        else:
+            messages.info(request, "Username or Password is incorrect")
+
+    context = {}
+    return render(request, 'register/login.html', context)
+
+def logoutUser(request): 
+    logout(request)
+    return redirect('login')
+
+    
