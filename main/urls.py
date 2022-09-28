ba73b16760dc8a7dbe45256e4b73a9371e8a3f30
alextaylor <alextaylor515@gmail.com>
2022-09-28 11:03:47 -0400

    Fixed merge conflicts, now running

--- main/urls.py
+++ main/urls.py
@@ -5,9 +5,6 @@ from django.urls import path
 
 urlpatterns = [
     path('',views.index, name="index"),
-<<<<<<< HEAD
-=======
     path('<int:id>', views.show, name="show"),
     path('create',views.create, name="create")
->>>>>>> 1eeed5938902ccad1a83c8c5bffce95f6c440e45
 ]
