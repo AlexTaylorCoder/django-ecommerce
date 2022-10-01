9acf925237d02a3b7192377ec09ba0c8c9936363
Hammad <hammadfaiz172@gmail.com>
2022-10-01 00:57:35 -0400

    almost done with product page

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,16 @@
 from django import forms 
+from django.forms import ModelForm
+from .models import Product
 
 class CreateNewProduct(forms.Form):
     name = forms.CharField(max_length=20)
     price = forms.DecimalField(max_digits = 8, decimal_places = 2)
     image = forms.ImageField(required=False)
+    miniDescription = forms.CharField(max_length=200, required=False)
+    description = forms.CharField(max_length=400, required=False)
+
+class ProductForm(ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
